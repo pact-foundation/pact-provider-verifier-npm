27f111e10f5b61dc8f366996f2951e60a7c878fb
Matt Fellows <matt.fellows@onegeek.com.au>
2016-05-12 22:49:01 +1000

    Working npm wrapper for pact-provider-verifier

--- scripts/build_standalone_packages.sh
+++ scripts/build_standalone_packages.sh
@@ -5,8 +5,7 @@ set -e
 mkdir -p build
 cd build
 if [ ! -d "pact-provider-verifier-${GEM_VERSION}" ]; then
-  # wget https://github.com/pact-foundation/pact-provider-verifier/archive/v${GEM_VERSION}.zip -O temp.zip
-  wget https://github.com/pact-foundation/pact-provider-verifier/archive/${GEM_VERSION}.zip -O temp.zip
+  wget https://github.com/pact-foundation/pact-provider-verifier/archive/v${GEM_VERSION}.zip -O temp.zip
   unzip temp.zip
   rm temp.zip
 fi
--- scripts/package_for_npm.sh
+++ scripts/package_for_npm.sh
@@ -17,6 +17,7 @@ else
 fi
 
 cp ../$SUFFIX/* $STANDALONE_PACKAGE_NAME
+chmod +x ../bin/pact-provider-verifier
 cp ../bin/pact-provider-verifier $STANDALONE_PACKAGE_NAME
 mv $STANDALONE_PACKAGE_NAME $NPM_PACKAGE_NAME
 cd $NPM_PACKAGE_NAME
